9ce5abd60dce25a351a340fd6b33026f8c1adadb
tomoxm <tomoxm@gmail.com>
2022-08-15 15:22:19 +0200

    new migration file for users

---
+++ database/migrations/2022_08_14_083254_create_uzivatelia_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uzivatelia', function (Blueprint $table) {
+            $table->id();
+            $table->string('login');
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('info');
+            $table->string('website');
+            $table->integer('type_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uzivatelia');
+    }
+};
